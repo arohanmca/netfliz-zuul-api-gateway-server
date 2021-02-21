b0b4f2cf7969f554b07b682cb60300f5df76935a
Rohan <arohan.mca@gmail.com>
2021-02-21 12:50:34 +0530

    netfliz-zuul-api-gateway-server

---
+++ src/test/java/com/rohan/microservices/netflizzuulapigatewayserver/NetflizZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohan.microservices.netflizzuulapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflizZuulApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
